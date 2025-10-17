14157a7b9d54fd73ff063854805f03d663289aa1
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-10-17 12:41:23 +0300

    Add 02 solution

---
+++ first_steps_in_coding_php_lab/02_numbers_from_1_to_10.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "1" . "\n";
+echo "2" . "\n";
+echo "3" . "\n";
+echo "4" . "\n";
+echo "5" . "\n";
+echo "6" . "\n";
+echo "7" . "\n";
+echo "8" . "\n";
+echo "9" . "\n";
+echo "10" . "\n";
