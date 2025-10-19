1b4ab7d4ff19e08e748b480846182e6ad96f8374
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-10-19 17:04:43 +0300

    Add solution for task 06

---
+++ first_steps_in_coding_php_lab/06_concatenate_text_and_digits.php
@@ -0,0 +1,8 @@
+<?php
+
+$first_name = readline();
+$last_name = readline();
+$age = readline();
+$town = readline();
+ 
+echo "You are " . $first_name . " " . $last_name . ", a " . $age . "-years old person from " . $town . ".";
