ec226cd4c859fe258b55b75b88940cd56cb1d5d3
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-10-21 16:07:08 +0300

    Add 07 solution

---
+++ first_steps_in_coding_php_lab/07_create_a_project.php
@@ -0,0 +1,6 @@
+<?php
+
+$name = readline();
+$number_of_projects = readline();
+
+printf('The architect %s will need %d hours to complete %d project/s.', $name, $number_of_projects * 3, $number_of_projects);
