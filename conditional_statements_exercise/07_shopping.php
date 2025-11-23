21bbe4d11553291c15bd1ac253f217cd4a4e18a3
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-11-23 21:19:52 +0200

    New solution

--- conditional_statements_exercise/07_shopping.php
+++ conditional_statements_exercise/07_shopping.php
@@ -1,3 +1,34 @@
 <?php
+$budget = floatval(readline());
+$video_cards = intval(readline());
+$processors = intval(readline());
+$ram = intval(readline());
 
+// Цени
+$video_price = 250;
+$total_video_price = $video_cards * $video_price;
+
+// Процесор: 35% от цената на всички видеокарти
+$processor_price = 0.35 * $total_video_price;
+$total_processor_price = $processors * $processor_price;
+
+// РАМ: 10% от цената на всички видеокарти
+$ram_price = 0.10 * $total_video_price;
+$total_ram_price = $ram * $ram_price;
+
+// Обща сума
+$total_sum = $total_video_price + $total_processor_price + $total_ram_price;
+
+// Проверка за отстъпка
+if ($video_cards > $processors) {
+    $total_sum *= 0.85; // -15%
+}
+
+if ($total_sum <= $budget) {
+    $money_left = $budget - $total_sum;
+    printf("You have %.2f leva left!", $money_left);
+} else {
+    $needed = $total_sum - $budget;
+    printf("Not enough money! You need %.2f leva more!", $needed);
+}
 ?>
